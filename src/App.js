783e9ce8c3a1753adf0f5bc418fb03f11d9b4861
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-03 09:54:57 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
+  // UI :: JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Home />
+      <AboutUs />
+    </>
+  );
+}
+
+function Home() {
+  return (
+    <div className="text-primary">
+      HOme Component Lorem ipsum, dolor sit amet consectetur adipisicing
+      elit. Recusandae explicabo ad perspiciatis alias, nisi molestias
+      libero sint officiis autem corrupti ipsam eum animi veniam voluptas
+      voluptates odio. Accusantium, odit itaque.
     </div>
   );
 }
 
-export default App;
+function AboutUs() {
+  return (
+    <div className="text-info">
+      About Us Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Architecto perferendis ut ullam placeat cumque vel porro, maxime quis
+      eveniet at a ipsam veniam aliquam incidunt, ratione veritatis minima.
+      Dolorem, hic!
+    </div>
+  );
+}
