db09f2c3902c7f5a5d840fe44b780a5c300ea08f
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-03 10:38:30 +0530

    basic of forms.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import { AboutUs } from "./views/AboutUs";
-import Home from "./views/Home";
-import { Product } from "./views/Product";
+import { useState } from "react";
 
 export default function App() {
   // UI :: JSX
   return (
     <>
-      <Home />
-      <Product />
-      <AboutUs />
+      <MyFormComponent />
     </>
+  );
+}
+
+function MyFormComponent() {
+  // statefull
+  const [username, setUsername] = useState("");
+
+  const onChangeUsername = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
+  return (
+    <div>
+      <h3>Working with Form Element</h3>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          value={username}
+          onChange={onChangeUsername}
+        />
+      </div>
+
+      <div>
+        <input type="password" placeholder="Enter Password" />
+      </div>
+    </div>
   );
 }
