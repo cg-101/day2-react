a6f652e7a521027263aa14518776de4ec38a8e5e
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-03 15:15:22 +0530

    with deve tools.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { MyReducer1 } from "./reducer1";
 import { MyReducer2 } from "./reducer2";
@@ -9,4 +10,7 @@ const reducers = combineReducers({
   reducer2: MyReducer2,
 });
 
-export const store = createStore(reducers, applyMiddleware(thunk));
+export const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
